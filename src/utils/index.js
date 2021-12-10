41203b0cc79a6d23b670089cb8ddaf046a575e5c
umair006 <umair.viralsquare@gmail.com>
2021-12-10 18:56:09 +0500

    data export through utils

---
+++ src/utils/index.js
@@ -0,0 +1,11 @@
+import  CONTRACTIONS_DATA  from "./ContractionsDataArray";
+import DESCENT_OF_HEAD from "./DescentOfDataArray";
+import HEART_RATE_DATA from "./HeartRateDataArray"
+import PULSE_DATA from "./PulseDataArray";
+
+export  {
+    CONTRACTIONS_DATA,
+    DESCENT_OF_HEAD,
+    HEART_RATE_DATA,
+    PULSE_DATA,
+}

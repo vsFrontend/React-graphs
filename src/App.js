83d40816145225a1b0a4504a30a32225217b509e
umair006 <umair.viralsquare@gmail.com>
2021-12-10 16:24:01 +0500

    react graphs

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import {
   Bar,
   XAxis,
   YAxis,
-  ReferenceArea,
   Legend,
   Tooltip,
   CartesianGrid,
   LineChart,
   Line,
   Cell,
-  ReferenceLine,
 } from "recharts";
 import "./App.css";
+import ReactCharts from "./components/chartjs.js/index";
 
 const heartRateData = [
   {
@@ -239,23 +238,26 @@ const pulsedata = [
   {
     name: "Page A",
     value: "80",
-    min: 80,
-    max: 130,
+    min: 280,
+    max: 230,
     pulserate: 90,
+    changeofValue: 4,
   },
   {
     name: "Page B",
     value: "90",
-    min: 80,
+    min: 380,
     max: 130,
     pulserate: 80,
+    changeofValue: 8,
   },
   {
     name: "Page C",
     value: "100",
-    min: 80,
+    min: 290,
     max: 130,
     pulserate: 116,
+    changeofValue: 9,
   },
   {
     name: "Page D",
@@ -263,6 +265,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 129,
+    changeofValue: 2,
   },
   {
     name: "Page E",
@@ -270,6 +273,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 120,
+    changeofValue: 8,
   },
   {
     name: "Page F",
@@ -277,6 +281,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 100,
+    changeofValue: 19,
   },
   {
     name: "Page G",
@@ -284,6 +289,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 120,
+    changeofValue: 10,
   },
   {
     name: "Page f",
@@ -291,6 +297,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 117,
+    changeofValue: 2,
   },
   {
     name: "Page h",
@@ -298,6 +305,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 119,
+    changeofValue: 100,
   },
   {
     name: "Page h",
@@ -305,6 +313,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 123,
+    changeofValue: 99,
   },
   {
     name: "Page h",
@@ -312,6 +321,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 104,
+    changeofValue: 8,
   },
   {
     name: "Page h",
@@ -319,6 +329,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 87,
+    changeofValue: 2,
   },
   {
     name: "Page h",
@@ -326,6 +337,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 90,
+    changeofValue: 3,
   },
   {
     name: "Page h",
@@ -333,6 +345,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 115,
+    changeofValue: 3,
   },
   {
     name: "Page h",
@@ -340,6 +353,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 100,
+    changeofValue: 99,
   },
   {
     name: "Page h",
@@ -354,6 +368,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 110,
+    changeofValue: 77,
   },
   {
     name: "Page h",
@@ -361,6 +376,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 112,
+    changeofValue: 99,
   },
   {
     name: "Page h",
@@ -368,6 +384,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 120,
+    changeofValue: 55,
   },
   {
     name: "Page h",
@@ -375,6 +392,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 130,
+    changeofValue: 22,
   },
   {
     name: "Page h",
@@ -382,6 +400,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 80,
+    changeofValue: 8,
   },
   {
     name: "Page h",
@@ -389,6 +408,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 100,
+    changeofValue: 8,
   },
   {
     name: "Page h",
@@ -396,6 +416,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 120,
+    changeofValue: 8,
   },
   {
     name: "Page h",
@@ -403,6 +424,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 130,
+    changeofValue: 66,
   },
   {
     name: "Page h",
@@ -410,6 +432,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 90,
+    changeofValue: 33,
   },
   {
     name: "Page h",
@@ -417,6 +440,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 119,
+    changeofValue: 8,
   },
   {
     name: "Page h",
@@ -424,6 +448,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 116,
+    changeofValue: 8,
   },
   {
     name: "Page h",
@@ -431,6 +456,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 88,
+    changeofValue: 44,
   },
   {
     name: "Page h",
@@ -438,6 +464,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 99,
+    changeofValue: 44,
   },
   {
     name: "Page h",
@@ -445,6 +472,7 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 122,
+    changeofValue: 2,
   },
   {
     name: "Page h",
@@ -452,6 +480,226 @@ const pulsedata = [
     min: 80,
     max: 130,
     pulserate: 118,
+    changeofValue: 3,
+  },
+];
+const decentofData = [
+  {
+    name: "Page A",
+    value: "4",
+    min: 0,
+    max: null,
+    heartrate: 100,
+  },
+  {
+    name: "Page B",
+    value: "5",
+    min: "1",
+    max: null,
+    heartrate: 100,
+  },
+  {
+    name: "Page C",
+    value: "6",
+    min: 2,
+    max: null,
+    heartrate: 116,
+  },
+  {
+    name: "Page D",
+    value: "7",
+    min: 3,
+    max: null,
+    heartrate: 139,
+  },
+  {
+    name: "Page E",
+    value: "8",
+    min: 4,
+    max: null,
+    heartrate: 100,
+  },
+  {
+    name: "Page F",
+    value: "9",
+    min: 5,
+    max: null,
+    heartrate: 100,
+  },
+  {
+    name: "Page G",
+    value: "10",
+    min: 6,
+    max: 2,
+    heartrate: 100,
+  },
+  {
+    name: "Page f",
+    value: "11",
+    min: 7,
+    max: 3,
+    heartrate: 177,
+  },
+  {
+    name: "Page h",
+    value: "12",
+    min: 8,
+    max: 4,
+    heartrate: 139,
+  },
+  {
+    name: "Page h",
+    value: "13",
+    min: 9,
+    max: 5,
+    heartrate: 143,
+  },
+  {
+    name: "Page h",
+    value: "14",
+    min: 10,
+    max: 6,
+    heartrate: 134,
+  },
+  {
+    name: "Page h",
+    value: "15",
+    min: 11,
+    max: 7,
+    heartrate: 127,
+  },
+  {
+    name: "Page h",
+    value: "16",
+    min: 12,
+    max: 8,
+    heartrate: 110,
+  },
+  {
+    name: "Page h",
+    value: "17",
+    min: 13,
+    max: 9,
+    heartrate: 115,
+  },
+  {
+    name: "Page h",
+    value: "18",
+    min: 14,
+    max: 10,
+    heartrate: 100,
+  },
+  {
+    name: "Page h",
+    value: "19",
+    min: 15,
+    max: 11,
+    heartrate: 155,
+  },
+  {
+    name: "Page h",
+    value: "20",
+    min: 16,
+    max: 11,
+    heartrate: 140,
+  },
+  {
+    name: "Page h",
+    value: "21",
+    min: 17,
+    max: 12,
+    heartrate: 112,
+  },
+  {
+    name: "Page h",
+    value: "22",
+    min: 18,
+    max: 13,
+    heartrate: 120,
+  },
+  {
+    name: "Page h",
+    value: "23",
+    min: 19,
+    max: 14,
+    heartrate: 130,
+  },
+  {
+    name: "Page h",
+    value: "24",
+    min: 20,
+    max: 15,
+    heartrate: 140,
+  },
+  {
+    name: "Page h",
+    value: "25",
+    min: 21,
+    max: 16,
+    heartrate: 150,
+  },
+  {
+    name: "Page h",
+    value: "26",
+    min: 22,
+    max: 17,
+    heartrate: 135,
+  },
+  {
+    name: "Page h",
+    value: "27",
+    min: 23,
+    max: 18,
+    heartrate: 145,
+  },
+  {
+    name: "Page h",
+    value: "28",
+    min: 24,
+    max: 19,
+    heartrate: 177,
+  },
+  {
+    name: "Page h",
+    value: "29",
+    min: 25,
+    max: 20,
+    heartrate: 166,
+  },
+  {
+    name: "Page h",
+    value: "30",
+    min: 26,
+    max: 21,
+    heartrate: 156,
+  },
+  {
+    name: "Page h",
+    value: "31",
+    min: 27,
+    max: 22,
+    heartrate: 144,
+  },
+  {
+    name: "Page h",
+    value: "32",
+    min: 28,
+    max: 23,
+    heartrate: 143,
+  },
+  {
+    name: "Page h",
+    value: "33",
+    min: 29,
+    max: 24,
+    heartrate: 122,
+  },
+  {
+    name: "Page h",
+    value: "36",
+    min: 30,
+    max: 25,
+    heartrate: 118,
   },
 ];
 
@@ -526,21 +774,54 @@ function App() {
 
       <h2 className="heading">Conditional bar chart</h2>
       <ResponsiveContainer width="100%" aspect="3">
-        <BarChart width={730} height={250} data={pulsedata}>
-        <CartesianGrid />
+        <BarChart stackOffset="sign" width={730} height={250} data={pulsedata}>
+          <CartesianGrid />
           <XAxis />
           <YAxis
             tickCount={80}
             domain={[0, 1000]}
             interval={"preserveStartEnd"}
           />
-          <Bar dataKey="value" fill="#00aeff" 
-                {...pulsedata.map((entry, index) => (
-                  <Cell fill={entry.value === "5" ? '#290a0a' : '#005599' }/>
-              ))}
-          />
+
+          <Bar dataKey="min" fill={"green"} />
+          <Bar dataKey="max" fill={"lightBlue"} />
         </BarChart>
       </ResponsiveContainer>
+
+      <h2 className="heading">Decent of head </h2>
+      <ResponsiveContainer width="100%" aspect={3}>
+        <LineChart
+          width={730}
+          height={250}
+          data={decentofData}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid />
+          <XAxis dataKey="min" interval={"preserveStartEnd"} />
+          <YAxis
+            ticks={[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+            domain={[0, 10]}
+            dataKey="value"
+            interval={"preserveStartEnd"}
+          />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="value"
+            stopColor="#3cb371"
+            strokeWidth={4}
+          />
+          <Line
+            type="monotone"
+            dataKey="max"
+            stopColor="#3cb371"
+            strokeWidth={4}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+
+      <ReactCharts />
     </>
   );
 }

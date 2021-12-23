112ca1fb8fbaf089109e8874a0a35a92af826bfc
vsFrontend <frontend@viralsquare.org>
2021-12-23 14:31:15 +0500

    shows 48 hours

--- src/components/PulseRate/index.js
+++ src/components/PulseRate/index.js
@@ -1,40 +1,48 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import ReactApexChart from "react-apexcharts";
 
 const PulseRate = () => {
-
+  const chart = useRef();
   const [pulse, setPulse] = useState([]);
   const [length, setLength] = useState([]);
-  
+
   const [actionLine, setActionLine] = useState(false);
   const [nullArrayData, setNullArrayData] = useState([]);
+  const [hourAgoData, setHourAgoData] = useState([])
 
-
-  useEffect(() => {
+  const getAllData = async () => {
     let getData = [];
     let lengthData = [];
     let nullArray = [];
-    const result = localStorage.getItem("MyData") || "{}";
+    let hourAgoArray = [];
+    const result = await localStorage.getItem("MyData") || "{}";
     const checkData = JSON.parse(result);
-    const convertToArray = Object.values(checkData);
+    const convertToArray = Object.values(checkData || []);
     convertToArray.map(item => {
       getData.push(item.Mpulse);
       lengthData.push(item.PcLength)
     });
-    
-    if (lengthData.includes(4)){
-      setActionLine(true)
-    }
-    let checkActionData = lengthData.findIndex(a => a === 4);
-    nullArray = new Array(checkActionData).fill(null);
-    convertToArray.map(item => {
+
+    convertToArray?.map(item => {
       lengthData.push(item.PcLength)
     });
+
+    if (lengthData?.includes(4)) {
+      setActionLine(true)
+    }
+    let checkActionData = lengthData?.findIndex(a => a === 4);
+    nullArray = new Array(checkActionData || 1)?.fill(null);
+    let twoHourAgoIndex = checkActionData + 4;
+    hourAgoArray = new Array(twoHourAgoIndex || 1).fill(null);
     setPulse(getData);
     setLength(lengthData);
     setNullArrayData(nullArray);
+    setHourAgoData(hourAgoArray);
+  }
 
-  }, [nullArrayData.length]);
+  useEffect(() => {
+    getAllData();
+  }, [nullArrayData?.length]);
 
 
   const seriesSet = [{
@@ -48,34 +56,48 @@ const PulseRate = () => {
   }, actionLine ? {
     name: 'Action',
     type: 'line',
-    data: [...nullArrayData,4,5,6,7,8,9,10]
-  } : {}];
+    data: [...nullArrayData, 4, 5, 6, 7, 8, 9, 10]
+  } : {},
+  
+  actionLine ? {
+    name: "Alert",
+    type: 'line',
+    data: [...hourAgoData, 4, 5, 6, 7, 8, 9, 10]
+  } : {},
+  {
+   
+    data:
+    [null , null, null, null, null, null, null, null,null , null, null, null,, null, null, null, null, null , null, null, null, null, null, null, null,null , null, null, null,, null, null, null, null, null , null, null, null, null, null, null, null,null , null, null, null, null]
+    },
+  ];
 
   const optionsSet = {
-
     chart: {
       height: 350,
       type: 'line',
       stacked: false,
       zoom: {
+        enabled: false,
         type: 'xy'
       }
     },
     stroke: {
-      width: [2, 5, 3],
-      curve: ['straight', 'stepline', 'straight']
+      width: [6, 5, 3, 4],
+      // colors: ['#F44336', '#E91E63', '#9C27B0'],
+      curve: ['straight', 'stepline', 'straight', 'straight']
     },
 
 
     annotations: {
+      // position: 'front' ,
+
       points: pulse.map((item, i) => {
-        
         return (
           {
-            x: item * item * 100,
+            x: item,
             y: item,
 
-            image: {
+            images: {
               path: 'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png',
               width: 20,
               height: 20,
@@ -85,6 +107,8 @@ const PulseRate = () => {
           }
         )
       })
+
+
     },
 
     grid: {
@@ -100,10 +124,17 @@ const PulseRate = () => {
         }
       },
     },
-    labels: ['00:00', '00:30', '01:00', '01:30', '02:00', '02:30', '03:00', '03:30', '04:00',
-      '04:30', '05:00', '05:30', '06:00', '06:30', '07:00', '07:30', '08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00', '22:30', '23:00', '23:30', '24:00', '24:30'
-    ],
-    
+    // labels: ['00:00', '00:30', '01:00', '01:30', '02:00', '02:30', '03:00', '03:30', '04:00',
+    //   '04:30', '05:00', '05:30', '06:00', '06:30', '07:00', '07:30', '08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00', '22:30', '23:00', '23:30', '24:00', '24:30'
+    // ],
+
+    markers:{
+      onClick: function(e) {
+        console.log("eee",e)
+      }
+    },
+
+
     // markers: {
     //   discrete: {
     //     seriesIndex: 0,
@@ -140,8 +171,13 @@ const PulseRate = () => {
     //   offsetY: -25,
     //   offsetX: -5
     // },
+
+    
     xaxis: {
-      type: 'time',
+      type: 'categories',
+      categories: ['00:00', '00:30', '01:00', '01:30', '02:00', '02:30','03:00', '03:30', '04:00', 
+      '04:30', '05:00', '05:30', '06:00', '06:30', '07:00', '07:30', '08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00', '22:30', '23:00', '23:30', '24:00', '24:30'],
+    
     },
     yaxis: {
       title: {
@@ -176,6 +212,7 @@ const PulseRate = () => {
         series={seriesSet}
         type="line"
         height={350}
+        ref={chart}
       />
     </div>
   )
